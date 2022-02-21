395bb795e9880bfcbbc58c5e8861bbc321d9a196
laba1 <sveta.007.512@yandex.ru>
2022-02-21 11:27:32 -0800

    push

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+
+a = int(input("а= " ))
+b = int(input("b= " ))
+c = int(input("с= " ))
+
+if int((b*b-4*a*c) < 0):
+    print('Корней нет')
+else:
+    d = math.sqrt(b*b-4*a*c)
+    if d > 0:
+        x1 = (-1 * b + d) / (a * 2)
+        x2 = (-1 * b - d) / (a * 2)
+        print(x1, x2, d)
+    else:
+        x1 = (-1 * b)/ (a * 2)
+        print('1 корень',x1)
